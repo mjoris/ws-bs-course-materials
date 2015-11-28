eca50ee9d2fb59f273e3f85c2175d29185fd67e5
Joris Maervoet <joris.maervoet@odisee.be>
2015-11-28 15:59:30 +0100

    Multiple additions and remarks on part 5

--- assets/05/examples/dirread.php
+++ assets/05/examples/dirread.php
@@ -9,24 +9,29 @@
 	$myBaseDir = __DIR__;
 
 	// open base directory
-	$dp = opendir($myBaseDir) or die('Error reading ' . $myBaseDir);
+	$dp = @opendir($myBaseDir);
 
-	// read base directory
-	while (($file = readdir($dp)) !== false) {
+	if ($dp !== false) {
+		// read base directory
+		while (($file = readdir($dp)) !== false) {
 
-		// exclude . and ..
-		if ($file == '.') continue;
-		if ($file == '..') continue;
+			// exclude . and ..
+			if ($file == '.') continue;
+			if ($file == '..') continue;
 
-		// we don't want directories
-		if (is_dir($myBaseDir . '/' . $file)) continue;
+			// we don't want directories
+			if (is_dir($myBaseDir . '/' . $file)) continue;
 
-		// output the filename
-		echo $file . '<br />' . PHP_EOL;
+			// output the filename
+			echo $file . '<br />' . PHP_EOL;
 
+		}
+
+		// close base directory pointer
+		closedir($dp);
+	} else {
+		echo ('Error reading ' . $myBaseDir);
 	}
 
-	// close base directory pointer
-	closedir($dp);
 
 // EOF
