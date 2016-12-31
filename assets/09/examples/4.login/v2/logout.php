af1a0b70d129c0377a5649836acc158bcc9df89a
Joris Maervoet <joris.maervoet@odisee.be>
2016-12-31 11:03:02 +0100

    Deleting unreachable HTML code

--- assets/09/examples/4.login/v2/logout.php
+++ assets/09/examples/4.login/v2/logout.php
@@ -12,26 +12,3 @@
 	// redirect to index
 	header('location: index.php');
 	exit();
-
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<title>My login protected site</title>
-
-	<meta charset="utf-8" />
-
-	<style>
-		.note {
-			background: #FFFF99;
-		}
-	</style>
-
-</head>
-<body>
-
-	<h1>My login protected site</h1>
-
-	<p>You are now logged out. <a href="index.php" title="Back to index">Back to index</a></p>
-
-</body>
-</html>
